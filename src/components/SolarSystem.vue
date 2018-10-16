a10b89b361dcd0d0db38a662ada4cb734eec5c85
tomganchos <tomganchos@mail.ru>
2018-10-17 00:02:48 +0300

    Create project, add Sun and Mercury

---
+++ src/components/SolarSystem.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="solar-system">
+        <div class="sun"><img src="https://png.icons8.com/color/50/000000/sun-star.png"></div>
+        <div class="mercury"><img src="https://png.icons8.com/color/50/000000/mercury-planet.png"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SolarSystem"
+    }
+</script>
+
+<style scoped>
+    @keyframes sun {
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg);
+        }
+    }
+    @keyframes mercury {
+        from {
+            transform: rotate(0deg) translate(-50px) rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg) translate(-50px) rotate(-720deg);
+        }
+    }
+    .solar-system {
+        height: calc(100vh - 16px);
+        background-color: #1c1c1c;
+        display: flex;
+    }
+    .sun {
+        display: flex;
+        position: absolute;
+        top: calc(50% - 25px);
+        left: calc(50% - 25px);
+        margin: auto;
+        animation-name: sun;
+        animation-duration: 3s;
+        animation-iteration-count: infinite;
+        animation-timing-function: linear;
+    }
+    .mercury {
+        display: flex;
+        position: absolute;
+        top: calc(50% - 5px);
+        left: calc(50% - 5px);
+        margin: auto;
+        animation-name: mercury;
+        animation-duration: 10s;
+        animation-iteration-count: infinite;
+        animation-timing-function: linear;
+    }
+    .mercury img {
+        width: 10px;
+        height: 10px;
+    }
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import SolarSystem from '@/components/SolarSystem'
 
 Vue.use(Router)
 
@@ -10,8 +11,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'solar-system',
+      component: SolarSystem
     },
     {
       path: '/about',
